47262d78eff9bf883f9b1da90fa264b689fe1c85
MontillaTomas <montilla.tom1@gmail.com>
2025-10-19 22:08:19 +0000

    Implement cache and add it to the api client

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/MontillaTomas/pokedexcli/internal/pokecache"
 )
 
 type Client struct {
 	httpClient http.Client
 	BaseURL    string
+	cache      *pokecache.Cache
 }
 
 func NewClient(timeout time.Duration) *Client {
@@ -18,6 +21,7 @@ func NewClient(timeout time.Duration) *Client {
 			Timeout: timeout,
 		},
 		BaseURL: "https://pokeapi.co/api/v2/",
+		cache:   pokecache.NewCache(5 * time.Second),
 	}
 }
 
@@ -27,6 +31,17 @@ func (c *Client) GetLocationAreas(url string) (*LocationAreaResponse, error) {
 		fullURL = c.BaseURL + "location-area/"
 	}
 
+	// Check cache first
+	if c.cache != nil {
+		if cachedData, found := c.cache.Get(fullURL); found {
+			var data LocationAreaResponse
+			if err := json.Unmarshal(cachedData, &data); err == nil {
+				return &data, nil
+			}
+		}
+	}
+
+	// Fetch from API
 	res, err := c.httpClient.Get(fullURL)
 	if err != nil {
 		return nil, err
@@ -40,6 +55,14 @@ func (c *Client) GetLocationAreas(url string) (*LocationAreaResponse, error) {
 	var data LocationAreaResponse
 	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
 		return nil, err
+	}
+
+	// Store in cache
+	if c.cache != nil {
+		rawData, err := json.Marshal(data)
+		if err == nil {
+			c.cache.Add(fullURL, rawData)
+		}
 	}
 
 	return &data, nil
