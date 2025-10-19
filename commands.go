6dc4ce1f1dad86f25d9cf5a23ca12ba2bacbd7b2
MontillaTomas <montilla.tom1@gmail.com>
2025-10-19 22:07:53 +0000

    Remove new line from print because it messed up the tests

--- commands.go
+++ commands.go
@@ -22,7 +22,8 @@ func commandExit() error {
 func commandHelp(commands map[string]cliCommand) func() error {
 	return func() error {
 		fmt.Println("Welcome to the Pokedex!")
-		fmt.Println("Usage:\n")
+		fmt.Println("Usage:")
+		fmt.Println("")
 		for _, cmd := range commands {
 			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 		}
